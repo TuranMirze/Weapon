6370e533f9d0a3fe097790c69fc3744ac853d2f8
TuranMirze <turanam-bp215@code.edu.az>
2024-10-19 08:58:07 +0400

    First

---
+++ Quiz/Program.cs
@@ -0,0 +1,165 @@
+﻿namespace Quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Weapon weapon = null;
+
+
+            Console.WriteLine("Silah Yaradin: ");
+            Console.Write("Silahin adi: ");
+            string Name = Console.ReadLine();
+
+            int capacity;
+            int bullet;
+            string firemode;
+
+            bool Iscapacity;
+            do
+            {
+                Console.Write("Umumi Gulle: ");
+                Iscapacity = int.TryParse(Console.ReadLine(), out capacity);
+
+                if (capacity > 0)
+                {
+                    Iscapacity = true;
+                }
+                else
+                {
+                    Console.WriteLine("Das atassan? Gulle qoy da!!");
+                    Iscapacity = false;
+                }
+            } while (!Iscapacity);
+
+            bool Isbullet;
+            do
+            {
+                Console.Write("Silahdaki gulle: ");
+                Isbullet = int.TryParse(Console.ReadLine(), out bullet);
+
+                if (capacity >= bullet)
+                {
+                    Isbullet = true;
+                }
+                else
+                {
+                    Console.WriteLine("Mermi sayini duz daxil edin");
+                    Isbullet = false;
+                }
+
+            }
+            while (!Isbullet);
+
+
+
+
+            Console.Write("Silah Modu (Tekli/Auto): ");
+            string mod = Console.ReadLine();
+
+            weapon = new Weapon(Name, capacity, bullet, mod);
+
+
+            int secim;
+
+            do
+            {
+                Console.WriteLine("Seçim edin:");
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Shoot metodu üçün");
+                Console.WriteLine("2 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("3 - Reload metodu üçün");
+                Console.WriteLine("4 - ChangeFireMode metodu üçün");
+                Console.WriteLine("5 - Proqramdan dayandırmaq üçün");
+                Console.WriteLine("6 - Edit");
+
+                secim = int.Parse(Console.ReadLine());
+
+                switch (secim)
+                {
+                    case 0:
+
+                        Console.WriteLine($"Capacity: {capacity}, Bullets: {bullet}, Mode: {mod}");
+                        break;
+          
+
+                    case 1:
+                        weapon.Shoot();
+                        break;
+
+                    case 2:
+                        weapon.GetRemainBulletCount();
+                        break;
+
+                    case 3:
+                        weapon.Reload();
+                        break;
+
+                    case 4:
+                        weapon.ChangeFireMode();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Proqram dayandırıldı.");
+                        return;
+                    case 6:
+                        int editSecim;
+                        do
+                        {
+                            Console.WriteLine("Edit Menu:");
+                            Console.WriteLine("7 - Capacityi deyisin");
+                            Console.WriteLine("8 - Gulle sayini deyisin");
+                            Console.WriteLine("9 - Esas menyuya geri qayidin");
+
+                            editSecim = int.Parse(Console.ReadLine());
+
+                            switch (editSecim)
+                            {
+                                case 7:
+                                    Console.Write("Capacityi daxil edin: ");
+                                    int newCapacity = int.Parse(Console.ReadLine());
+                                    if (newCapacity < weapon.Bullet)
+                                    {
+                                        Console.WriteLine("Capacity gulle sayindan az ola bilmez.");
+                                    }
+                                    else
+                                    {
+                                        weapon.Capacity = newCapacity;
+                                        Console.WriteLine("Capacity deyisdi: " + weapon.Capacity);
+                                    }
+                                    break;
+                                     
+                                case 8:
+                                    Console.Write("Yeni gulle sayini daxil edin: ");
+                                    int newBullet = int.Parse(Console.ReadLine());
+                                    if (newBullet > weapon.Capacity)
+                                    {
+                                        Console.WriteLine("Gulle sayi capacityden cox ola bilmez.");
+                                    }
+                                    else
+                                    {
+                                        weapon.Bullet = newBullet;
+                                        Console.WriteLine("Yeni gulle sayi: " + weapon.Bullet);
+                                    }
+                                    break;
+                                case 9:
+                                    break;
+                                default:
+                                    Console.WriteLine("Sehvdir. Yeniden yoxlayin");
+                                    break;
+                            }
+                        } while (editSecim != 9);
+
+                        break;
+                    default:
+
+                        Console.WriteLine("Yanlış seçim!");
+                        break;
+
+                }
+            } while (secim != 5);
+        }
+    }
+}
---
+++ Quiz/Weapon.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Quiz
+{
+    internal class Weapon
+    {
+        public string Name;
+        private int _capacity;
+        private int _bullet;
+        public string FireMod;
+
+
+
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _capacity = value;
+                }
+            }
+
+        }
+        
+
+        public int Bullet
+        {
+            get { 
+
+                return _bullet; 
+
+                }
+
+            set {
+
+                if(value>0) {                               
+                    _bullet = value;
+                }
+        }
+    }
+
+       
+
+
+        public Weapon(string name, int capacity, int bullet, string firemod) {
+
+            this.Name= name;
+            _bullet = bullet;
+            _capacity = capacity;
+            this.FireMod = firemod;
+
+
+        }
+        public void Shoot()
+        {
+
+            if (Bullet > 0 && FireMod == "Tekli")
+            {
+                Bullet--;
+                Console.WriteLine("1 gulle atildi. Qalan gulle: " + Bullet);
+            }
+            else if (Bullet >= 0 && FireMod == "Avto")
+            {
+                Bullet = 0;
+                Console.WriteLine("Butun sarsor bosaldildi. Reload edin");
+            }
+            else {
+                Console.WriteLine("Duz olmadi nese");
+            }
+
+        }
+
+
+
+        public void GetRemainBulletCount()
+        {
+            if (Capacity>=Bullet)
+            {
+                int qalan = Capacity - Bullet;
+                Console.WriteLine("Mermin" + " " + Bullet + " " + "denedir");
+                Console.WriteLine( qalan + "  mermi de qoysan daraq dolacax");
+            }
+            else
+            {
+                Console.WriteLine("Mermi sayin duz yazmamisan");
+            }
+        }
+
+
+        public void Reload()
+        {
+            if (Capacity >= Bullet)
+            {
+                Bullet = Capacity;
+                Console.WriteLine("Silahi reload edildi. Qalan gulle: " + Bullet);
+            }
+            else {
+                Console.WriteLine("Nese prob oldu");
+            }
+        }
+
+
+        public void ChangeFireMode()
+        {
+            if (FireMod == "Tekli")
+            {
+                FireMod = "Auto";
+                Console.WriteLine("Silahin avto moddadi. Tara getsin");
+            }
+
+            else if(FireMod =="Avto")
+            {
+                FireMod = "Tekli";
+                Console.WriteLine("Silahin tekli moddadi. Kafadan vur");
+            }
+            else
+            {
+                Console.WriteLine("Modu daxil edin"); 
+            }
+        }
+
+    }
+}
